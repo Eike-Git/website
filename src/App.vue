fd062a61732a049976746bf5fc147fc6cbbbc9e8
Eike Steinwachs <eike@123steinwachs.de>
2024-04-21 15:00:43 +0200

    axios test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <h1>Filmliste</h1>
+    <ul>
+      <li v-for="Filme in Filmliste" :key="filme.field_film_titel">{{ filme.field_film_beschreibung }}</li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
---
+++ src/components/FilmListe.vue
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+
+export default {
+  data() {
+    return {
+      filme: []
+    };
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:32804/api/filme')
+      .then(response => {
+        this.filme = response.data;
+      })
+      .catch(error => {
+        console.error('Fehler beim Abrufen der Filmliste:', error);
+      });
+  }
+}
