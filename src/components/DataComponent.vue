499b0297ecdd3dbb4a8c9a72826a34c744cb1ceb
Eike Steinwachs <eike@123steinwachs.de>
2024-04-22 10:57:00 +0200

    axios await

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -2,7 +2,9 @@
   <div>
     <h1>Daten von der lokalen JSON-API</h1>
     <ul v-if="dataFromApi">
-      <li v-for="item in dataFromApi" :key="item.field_film_id">{{ item.field_film_titel }}</li>
+      <li v-for="item in dataFromApi" :key="item.field_film_id">
+        {{ item.field_film_titel }}
+      </li>
     </ul>
     <p v-else>Loading...</p>
   </div>
@@ -12,19 +14,21 @@
 import axios from 'axios';
 
 export default {
+  name: "FilmListe",
   data() {
     return {
       dataFromApi: []
     };
   },
-  mounted() {
-    axios.get('https://easy-weekly-werewolf.ngrok-free.app/api/filme')
-      .then(response => {
-        this.dataFromApi = response.data;
-      })
-      .catch(error => {
-        console.error('Fehler beim Abrufen der Daten:', error);
-      });
+  async created() {
+    try {
+      const res = await axios.get('https://easy-weekly-werewolf.ngrok-free.app/api/filme')
+      
+      this.dataFromApi = res.data;
+    }catch(e) {
+      console.error(e);
+    }
   }
 };
+
 </script>
