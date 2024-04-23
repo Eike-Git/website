e43adcbd344af36795e7b20a70f5bac05b339973
Eike Steinwachs <website_user@drupal.example.com>
2024-04-23 10:08:20 +0200

    Revert "typescript"
    This reverts commit d5cea7f340c8ee1c813c7c4c43528e9af3e07f5a.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Film-Liste />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import FilmListe from "./components/DataComponent.vue";
 
-export default defineComponent({
-  name: 'App',
+export default {
+  name: "App",
   components: {
-    HelloWorld
+    FilmListe
   }
-});
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
