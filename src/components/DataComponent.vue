86ebe8b1df55a2ff02c54cad317ff3d242528351
Eike Steinwachs <eike@123steinwachs.de>
2024-04-23 13:25:11 +0200

    full list

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -4,7 +4,13 @@
     <ul v-if="dataFromApi">
       <li v-for="item in dataFromApi" :key="item.field_film_id">
         {{ item.field_film_titel }}
+        {{ item.field_film_beschreibung }}
+        {{ item.field_film_erscheinungsdatum }}
         {{ item.field_film_poster }}
+        {{ item.field_film_genre }}
+        {{ item.field_film_hauptdarsteller_in_1 }}
+        {{ item.field_film_hauptdarsteller_in_2 }}
+        {{ item.field_film_regisseur_in }}
       </li>
     </ul>
     <p v-else>Loading...</p>
