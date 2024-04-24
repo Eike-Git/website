b2f7fa3e53b534c7cd9e5e7c6adee8b7699b0711
Eike Steinwachs <eike@123steinwachs.de>
2024-04-24 11:32:38 +0200

    ${url}

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -10,7 +10,7 @@
         {{ item.field_film_hauptdarsteller_in_1 }}
         {{ item.field_film_hauptdarsteller_in_2 }}
         {{ item.field_film_regisseur_in }}
-        <img :src="url+item.field_film_poster" style="width:100%;" alt="alternative">
+        <img :src="${url}+item.field_film_poster" style="width:100%;" alt="alternative">
       </li>
     </ul>
     <p v-else>Loading...</p>
