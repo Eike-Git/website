65390841d00d2caca02cf344003145e7a2777216
Eike Steinwachs <eike@123steinwachs.de>
2024-04-24 11:40:53 +0200

    doch keine variable

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -10,7 +10,7 @@
         {{ item.field_film_hauptdarsteller_in_1 }}
         {{ item.field_film_hauptdarsteller_in_2 }}
         {{ item.field_film_regisseur_in }}
-        <img :src="${url}+item.field_film_poster" style="width:100%;" alt="alternative">
+        <img :src="'https://easy-weekly-werewolf.ngrok-free.app'+item.field_film_poster" style="width:100%;" alt="alternative">
       </li>
     </ul>
     <p v-else>Loading...</p>
@@ -20,8 +20,6 @@
 <script>
 import axios from 'axios';
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = `1`;
-
-const url = 'https://easy-weekly-werewolf.ngrok-free.app';
 
 export default {
   name: "FilmListe",
