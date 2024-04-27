d6279354ada87d5cfd7a4bbc635e513783a85b82
Eike Steinwachs <eike@123steinwachs.de>
2024-04-27 18:10:24 +0200

    image rendering

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -4,7 +4,7 @@
     <ul v-if="dataFromApi">
       <li v-for="item in dataFromApi" :key="item.id">
         {{ item.name}}
-        {{ item.imageURL }}
+        img :src='{{ item.imageURL }}''
         {{ item.synopsis }}
         {{ item.year }}
         {{ item.genre }}        
