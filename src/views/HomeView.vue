4adb4a51b577ad20f70550054bce2107c37be92e
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-05-08 13:02:42 +0200

    import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MoviesApi/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MoviesApi from '@/components/DataComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MoviesApi
   }
 }
 </script>
