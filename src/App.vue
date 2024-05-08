17e3396586eb007e728728a1975d13e1ba6ea0f7
maxkessel <vue_user@vuejs.eike.mylocal>
2024-05-08 07:18:40 +0000

    t

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <template>
-  <!-- <Film-Liste/> -->
-  <div class="container">
-    <div class="row">
-      <div class="col-1">
-        <nav class="navbar">
-          <h1>test</h1>
-          <button><img src="" alt=""></button>
-          <button><img src="" alt=""></button>
-          <button><img src="" alt=""></button>
-          <button><img src="" alt=""></button>
-        </nav>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-2"></div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-1"></div>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script lang="ts">
-import FilmListe from "./components/DataComponent.vue";
-
-export default({
-  name: 'App',
-  components: {
-    FilmListe
-  }
-});
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
