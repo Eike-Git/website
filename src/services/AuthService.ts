80339a2d03b07154933397a51501a4c63be5599f
Eike-Git <eike@123steinwachs.de>
2024-06-07 10:33:11 +0200

    auth service error messages

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,7 @@
 // src/services/authService.ts
 import axios from 'axios';
 
-const API_URL = 'http://100.68.230.120:1337'; // URL deiner Strapi-API
+const API_URL = 'http://100.68.230.120:1337'; // URL Strapi-API
 
 interface RegisterResponse {
   jwt: string;
@@ -22,18 +22,38 @@ interface LoginResponse {
 }
 
 export const register = async (username: string, email: string, password: string): Promise<RegisterResponse> => {
-  const response = await axios.post<RegisterResponse>(`${API_URL}/auth/local/register`, {
-    username,
-    email,
-    password,
-  });
-  return response.data;
+  try {
+    const response = await axios.post<RegisterResponse>(`${API_URL}/auth/local/register`, {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error('Registration failed:', error.response.data);
+      throw new Error(`Registration failed: ${error.response.data.message}`);
+    } else {
+      console.error('Unexpected error:', error);
+      throw new Error('Unexpected error occurred during registration');
+    }
+  }
 };
 
 export const login = async (identifier: string, password: string): Promise<LoginResponse> => {
-  const response = await axios.post<LoginResponse>(`${API_URL}/auth/local`, {
-    identifier,
-    password,
-  });
-  return response.data;
+  try {
+    const response = await axios.post<LoginResponse>(`${API_URL}/auth/local`, {
+      identifier,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error('Login failed:', error.response.data);
+      throw new Error(`Login failed: ${error.response.data.message}`);
+    } else {
+      console.error('Unexpected error:', error);
+      throw new Error('Unexpected error occurred during login');
+    }
+  }
 };
