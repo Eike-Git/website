711608dbaa488e0d143b4b561cfdc539835766d0
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-06-24 15:40:48 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,7 @@
 <template>
-  <div class="background">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <nav class="navbar">
-            <router-link to="/" src="src/assets/IconHome.png"></router-link>
-            <router-link to="/" src="src/assets/IconFilter.png"></router-link>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <router-view/>
-  </div>
+
 </template>
 
 <style>
-/* .background {
-  background-image: url('/src/assets/backgroundHome.png'); 
-  background-size: cover;
-  background-position: center;
-  position: relative;
-  z-index: 1;
-  height: 100vh;
-  tag="img"
-} */
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-nav {
-  padding: 30px;
-}
-.navbar > router-link img {
-  width: 30px;
-  height: 30px;
-}
+
 </style>
