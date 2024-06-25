a8be4ff5c37cfce8b5db8f3f357d87ab73849c51
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-06-25 10:11:54 +0200

    icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <MoviesApi/>
   </div>
+  <HeaderSlider></HeaderSlider>
 </template>
 
 <script>
 // @ is an alias to /src
 import MoviesApi from '@/components/DataComponent.vue'
+import HeaderSlider from '@/components/HeaderSlider.vue'; 
 
 export default {
   name: 'HomeView',
   components: {
-    MoviesApi
+    MoviesApi,
+    HeaderSlider
   }
 }
 </script>
