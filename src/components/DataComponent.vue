ec7fda287bffb720418c44c3583a80dadf15aa59
D-VELOP\ckur <ckur@d-velop.de>
2024-06-28 12:21:01 +0200

    Filter Code eingebaut

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <div v-if="dataFromApi" class="film-grid">
-      <MovieVisuals v-for="film in dataFromApi" :key="film.id" :film="film" />
+    <DropdownFilter @genre-changed="applyGenreFilter" />
+    <div v-if="filteredData.length > 0" class="film-grid">
+      <MovieVisuals v-for="film in filteredData" :key="film.id" :film="film" />
     </div>
     <p v-else>Loading...</p>
   </div>
@@ -10,22 +11,40 @@
 <script>
 import axios from 'axios';
 import MovieVisuals from './MovieVisuals.vue';
+import DropdownFilter from './DropdownFilter.vue';
 
 export default {
   name: "DataComponent",
   components: {
-    MovieVisuals
+    MovieVisuals,
+    DropdownFilter
   },
   data() {
     return {
-      dataFromApi: []
+      dataFromApi: [],
+      filteredData: [],
+      selectedGenre: ""
     };
+  },
+  methods: {
+    applyGenreFilter(genre) {
+      this.selectedGenre = genre;
+      this.filterData();
+    },
+    filterData() {
+      if (this.selectedGenre === "") {
+        this.filteredData = this.dataFromApi;
+      } else {
+        this.filteredData = this.dataFromApi.filter(film => film.genre === this.selectedGenre);
+      }
+    }
   },
   async created() {
     try {
       const res = await axios.get('http://100.68.230.120:1337/movies');
       this.dataFromApi = res.data;
-    } catch(e) {
+      this.filteredData = this.dataFromApi; // Initial set to all data
+    } catch (e) {
       console.error(e);
     }
   }
--- src/components/DropdownFilter.vue
+++ src/components/DropdownFilter.vue
@@ -1,33 +1,40 @@
 <template>
-    <div>
-      <label for="genre-filter">Filter nach Genre</label>
-      <select id="genre-filter" v-model="selectedGenre">
-        <option v-for="(genre, index) in genres" :key="index" :value="genre.value">{{ genre.label }}</option>
-      </select>
-      <p>Ausgewähltes Genre: {{ selectedGenre }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'DropdownFilter',
-    data() {
-      return {
-        selectedGenre: null,
-        genres: [
-          { value: 'action', label: 'Action' },
-          { value: 'comedy', label: 'Comedy' },
-          { value: 'drama', label: 'Drama' },
-          { value: 'horror', label: 'Horror' },
-          { value: 'sci-fi', label: 'Sci-Fi' },
-          { value: 'thriller', label: 'Thriller' }
-        ]
-      };
+  <div>
+    <label for="genre-filter">Filter nach Genre</label>
+    <select id="genre-filter" v-model="selectedGenre" @change="updateSelectedGenre">
+      <option v-for="(genre, index) in genres" :key="index" :value="genre.value">
+        {{ genre.label }}
+      </option>
+    </select>
+    <p>Ausgewähltes Genre: {{ selectedGenre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownFilter',
+  data() {
+    return {
+      selectedGenre: null,
+      genres: [
+        { value: '', label: 'Alle Genres' },
+        { value: 'action', label: 'Action' },
+        { value: 'comedy', label: 'Comedy' },
+        { value: 'drama', label: 'Drama' },
+        { value: 'horror', label: 'Horror' },
+        { value: 'science-fiction', label: 'Science Fiction' },
+        { value: 'thriller', label: 'Thriller' }
+      ]
+    };
+  },
+  methods: {
+    updateSelectedGenre() {
+      this.$emit('genre-changed', this.selectedGenre);
     }
-  };
-  </script>
-  
-  <style scoped>
-  /* Stile für die Dropdown-Komponente */
-  </style>
-  
+  }
+};
+</script>
+
+<style scoped>
+/* Stile für die Dropdown-Komponente */
+</style>
