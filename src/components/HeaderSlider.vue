1cce32c388e7867430de374f5672eaca51a72cad
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-07-09 14:29:40 +0200

    Header neu

--- src/components/HeaderSlider.vue
+++ src/components/HeaderSlider.vue
@@ -1,12 +1,13 @@
 <template>
-  <header class="flex-header">
+  <header class="flex-header" v-if="currentFilm">
     <div class="left-column">
       <h1>{{ currentFilm.name }}</h1>
     </div>
     <div class="right-column">
-      <img v-if="currentFilm" :src="currentFilm.imageURL" alt="Film Image" class="film-image" />
+      <img :src="currentFilm.imageURL" alt="Film Image" class="film-image" />
     </div>
   </header>
+  <p v-else>Loading...</p>
 </template>
 
 <script>
@@ -30,7 +31,9 @@ export default {
       try {
         const res = await axios.get('http://100.68.230.120:1337/movies');
         this.films = res.data;
-        this.startFilmRotation();
+        if (this.films.length > 0) {
+          this.startFilmRotation();
+        }
       } catch (e) {
         console.error(e);
       }
@@ -38,7 +41,7 @@ export default {
     startFilmRotation() {
       setInterval(() => {
         this.currentFilmIndex = (this.currentFilmIndex + 1) % this.films.length;
-      }, 10000); 
+      }, 10000); // Alle 10 Sekunden wechseln
     }
   },
   created() {
