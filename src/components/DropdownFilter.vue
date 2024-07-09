9acdd98dc0436bd39d42dd59c07401f685126cc2
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-07-09 13:00:06 +0200

    Header neu

--- src/components/DropdownFilter.vue
+++ src/components/DropdownFilter.vue
@@ -44,9 +44,8 @@ export default {
  margin: 0 0 0 1rem;
  background-color: rgba(28, 28, 28, 0.6);
  padding: 5px;
- border: none;
  border-radius: 8px;
- border-bottom: 1px solid #ccc; 
+ border: 1px solid #ccc; 
  color: #e7e7e7;
 }
 
