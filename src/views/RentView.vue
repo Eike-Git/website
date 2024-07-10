3ecac14a0e7a7a91cb31037fa1c6360e1cdf0987
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-07-10 13:20:43 +0200

    Rent Formular

--- src/views/RentView.vue
+++ src/views/RentView.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
       <h1>Film ausleihen: {{ name }}</h1>
+      <p><strong>Genre:</strong> {{ film.genre }}</p>
+      <p><strong>Year:</strong> {{ film.year }}</p>
+      <p>{{ film.synopsis }}</p>
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
   
   export default {
     name: 'RentView',
@@ -14,6 +18,14 @@
         film: {}
       };
     },
+    async created() {
+      try {
+        const res = await axios.get(`http://100.68.230.120:1337/movies/${this.id}`);
+        this.film = res.data;
+      } catch (e) {
+        console.error(e);
+      }
+    }
   };
   </script>
   
