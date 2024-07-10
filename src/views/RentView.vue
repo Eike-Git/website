85c9f8cb7df612130cfb675f2ca344a1595b3850
Max Kesselmann <maxkesselmann@MacBook-Pro-von-Max.local>
2024-07-10 14:59:51 +0200

    Rent Formular

--- src/views/RentView.vue
+++ src/views/RentView.vue
@@ -1,88 +1,119 @@
 <template>
     <div>
       <h2>{{ name }}</h2>
-    </div>
-    <div class="form-container">
+      <div class="form-container">
         <form @submit.prevent="submitForm">
-        <div class="form">
+          <div class="form">
             <label for="firstName" class="formLabel">Vorname</label>
-            <input type="text" class="formControl" id="firstName" v-model="firstName" required>
-        </div>
-        <div class="form">
+            <input type="text" class="formControl" id="firstName" v-model="firstName" placeholder="Vorname" required>
+          </div>
+          <div class="form">
             <label for="lastName" class="formLabel">Nachname</label>
-            <input type="text" class="formControl" id="lastName" v-model="lastName" required>
-        </div>
-        <div class="form">
+            <input type="text" class="formControl" id="lastName" v-model="lastName" placeholder="Nachname" required>
+          </div>
+          <div class="form">
             <label for="street" class="formLabel">Straße</label>
-            <input type="text" class="formControl" id="street" v-model="street" required>
-        </div>
-        <div class="form">
+            <input type="text" class="formControl" id="street" v-model="street" placeholder="Straße" required>
+          </div>
+          <div class="form">
             <label for="city" class="formLabel">Ort</label>
-            <input type="text" class="formControl" id="city" v-model="city" required>
-        </div>
-        <div class="form">
+            <input type="text" class="formControl" id="city" v-model="city" placeholder="Ort" required>
+          </div>
+          <div class="form">
             <label for="plz" class="formLabel">PLZ</label>
-            <input type="text" class="formControl" id="plz" v-model="plz" required>
-        </div>
-        <div class="form">
+            <input type="text" class="formControl" id="plz" v-model="plz" placeholder="PLZ" required>
+          </div>
+          <div class="form">
             <label for="rentalDates" class="formLabel">Ausleihdatum von bis</label>
             <div class="row">
-            <div class="col-sm-6">
-                <input type="date" class="formControl" id="rentalFrom" v-model="rentalFrom" required>
+              <div class="col-sm-6">
+                <input type="date" class="formControl" id="rentalFrom" v-model="rentalFrom" placeholder="Ausleihdatum von" required>
+              </div>
+              <div class="col-sm-6 mt-2 mt-sm-0">
+                <input type="date" class="formControl" id="rentalTo" v-model="rentalTo" placeholder="Ausleihdatum bis" required>
+              </div>
             </div>
-            <div class="col-sm-6 mt-2 mt-sm-0">
-                <input type="date" class="formControl" id="rentalTo" v-model="rentalTo" required>
-            </div>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Formular absenden</button>
+          </div>
+          <button type="submit" class="btn btn-primary">Formular absenden</button>
         </form>
-  </div>
+      </div>
+    </div>
   </template>
   
   <script>
   export default {
     name: 'RentView',
     props: ['name', 'id'],
-    mounted() {
-        console.log('Prop "name" received:', this.name, 'id:', this.id);
-    },
     data() {
-        return {
-            firstName: '',
-            lastName: '',
-            street: '',
-            city: '',
-            plz: '',
-            rentalFrom: '',
-            rentalTo: ''
-        };
-    },  
+      return {
+        firstName: '',
+        lastName: '',
+        street: '',
+        city: '',
+        plz: '',
+        rentalFrom: '',
+        rentalTo: ''
+      };
+    },
     methods: {
-        name: 'MinimalForm',
-        submitForm() 
-        {
-            console.log('Form submitted with data:', {
-                firstName: this.firstName,
-                lastName: this.lastName,
-                street: this.street,
-                city: this.city,
-                plz: this.plz,
-                rentalFrom: this.rentalFrom,
-                rentalTo: this.rentalTo
-            });
-            this.firstName = '';
-            this.lastName = '';
-            this.street = '';
-            this.city = '';
-            this.plz = '';
-            this.rentalFrom = '';
-            this.rentalTo = '';
-        }
-  }
+      submitForm() {
+        console.log('Form submitted with data:', {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          street: this.street,
+          city: this.city,
+          plz: this.plz,
+          rentalFrom: this.rentalFrom,
+          rentalTo: this.rentalTo
+        });
+        this.firstName = '';
+        this.lastName = '';
+        this.street = '';
+        this.city = '';
+        this.plz = '';
+        this.rentalFrom = '';
+        this.rentalTo = '';
+      }
+    }
   };
   </script>
   
   <style scoped>
+  .form-container {
+    margin-top: 20px;
+    padding: 20px;
+    background-color: rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+  }
+  
+  .form {
+    margin-bottom: 16px;
+  }
+  
+  .formLabel {
+    font-size: 16px;
+    color: #333;
+  }
+  
+  .formControl {
+    width: 100%;
+    padding: 8px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  .btn-primary {
+    background-color: #007bff;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  .btn-primary:hover {
+    background-color: #0056b3;
+  }
   </style>
   
